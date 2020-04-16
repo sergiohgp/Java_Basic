01d5cac399a83fde47ae7a2c1178cdeab7cada99
Sergio Pereira <54996062+sergiohgp@users.noreply.github.com>
2020-04-16 00:01:31 -0400

    Add files via upload

---
+++ Week1/src/week1.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+/**
+ * @author sergiohgp
+ *
+ */
+public class week1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Declare Values
+		String name = "Sergio Pereira";
+		int age = 28;
+		
+		//Print name and age
+		System.out.println("My name is "+name+ " and my age is "+age);
+
+	}
+
+}
