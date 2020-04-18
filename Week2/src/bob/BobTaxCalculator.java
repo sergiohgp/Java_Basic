835760d9c88c6dcbf5a53dae7251e1ec2e2430c6
Sergio Pereira <54996062+sergiohgp@users.noreply.github.com>
2020-04-17 20:13:35 -0400

    Add files via upload

---
+++ Week2/src/bob/BobTaxCalculator.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package bob;
+import javax.swing.JOptionPane;
+
+/**
+ * @author sergiohgp
+ *
+ */
+public class BobTaxCalculator {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+			double taxRate = 0.26;
+			double bobSalary = 120000;
+			double taxOwed = taxRate * bobSalary;
+			double finalSalary = bobSalary - taxOwed;
+			
+			// Output
+			System.out.println("Bob owes $"+taxOwed+" in taxes and " + "Bob's take home pay is $"+finalSalary);
+
+		}
+
+	
+
+	
+
+}
